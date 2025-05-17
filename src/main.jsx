f0e7cfae35ea06c1d9138c3e7088246a144a03e3
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-17 20:33:25 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import PrivacyPolicy from './pages/PrivacyPolicy'; // crie este componente
+import Status from './pages/StatusSection';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/privacidade" element={<PrivacyPolicy />} />
+        <Route path="/status" element={<Status />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
