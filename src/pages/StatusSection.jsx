34a604a069aa3bc89e1f3c85564a3c25f28bef00
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-17 22:17:54 -0300

    Update StatusSection.jsx

--- src/pages/StatusSection.jsx
+++ src/pages/StatusSection.jsx
@@ -1,7 +1,6 @@
 const StatusSection = () => {
   return (
     <section className="w-full h-screen">
-      <h2 className="text-2xl font-bold mb-4 text-center">Status dos Serviços</h2>
       <div className="w-full h-[80vh]">
         <iframe
           src="https://services.dkdevs.com.br"
